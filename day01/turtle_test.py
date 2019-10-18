feaac38e4639df910097ccdebf9a20c55dcfafa5
penghui <penghui@diyidan.com>
2019-10-18 17:47:16 +0800

    10-18

---
+++ day01/turtle_test.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# -*- author: 彭慧 -*-
+
+import turtle
+import time
+
+"""正方形"""
+# turtle.pensize(4)
+# turtle.pencolor('red')
+#
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+
+
+
+"""sun flower"""
+# turtle.color('red', 'yellow')
+# turtle.begin_fill()
+# for i in range(50):
+#     turtle.forward(200)
+#     turtle.left(170)
+# turtle.end_fill()
+
+
+
+"""五角星"""
+# turtle.pensize(10)
+# turtle.pencolor('yellow')
+# turtle.fillcolor('red')
+# turtle.begin_fill()
+# for i in range(5):
+#     turtle.forward(150)
+#     turtle.left(144)
+#
+# turtle.end_fill()
+# time.sleep(2)
+#
+# turtle.penup()
+# turtle.goto(-150, -200)
+# turtle.color('red')
+# turtle.write('I love China', font=('Arial', 40, 'normal'))
+
+
+turtle.mainloop()
